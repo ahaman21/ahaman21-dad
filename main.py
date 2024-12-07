8bb56636ccee70c1fd84453b54a0e334378e18d1
Ahmet Haman <ahmethaman2001@gmail.com>
2024-12-07 17:42:32 +0300

    daha eklendi

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import math
 print("3234")
 
-print(2234+45e2)
+
 print(math.pi)
 print(math.sin(math.pi/2))
 print(math.cos(math.pi/2))
+print(math.tan(math.pi/2))
+print(math.tan(45))
+print(math.tan(math.pi/4))
+print(math.tan(math.pi/2))
